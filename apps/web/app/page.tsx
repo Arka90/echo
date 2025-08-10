9b0a52aa8c68ddef9e1b2f7783644738ce382a33
arka90 <skarka90@gmail.com>
2025-08-10 22:19:36 +0530

    feat: added turborepo

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,14 @@
-import { Button } from "@workspace/ui/components/button"
+import { Button } from "@workspace/ui/components/button";
+import { add } from "@workspace/math/add";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
+        <h1 className="text-2xl font-bold">Hello apps/page</h1>
+        <p className="text-lg">2 + 3 = {add(2, 3)}</p>
         <Button size="sm">Button</Button>
       </div>
     </div>
-  )
+  );
 }
