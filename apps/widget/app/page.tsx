19b74cbf47a8d3dd4b1842737724b66dbb894617
arka90 <skarka90@gmail.com>
2025-08-15 21:55:34 +0530

    feat: added widget header and footer

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,31 +1,14 @@
 "use client";
-import { Button } from "@workspace/ui/components/button";
-import { useQuery } from "convex/react";
-import { api } from "@workspace/backend/_generated/api";
-import { useVapi } from "@/modules/widget/hooks/use-vapi";
+import { WidgetView } from "@/modules/widget/ui/views/widget-view";
+import { use } from "react";
 
-export default function Page() {
-  const users = useQuery(api.users.getMany);
-  const {
-    isConnected,
-    isConnecting,
-    isSpeaking,
-    transcript,
-    startCall,
-    endCall,
-  } = useVapi();
-  return (
-    <div className="flex items-center justify-center min-h-svh">
-      <Button onClick={() => startCall()} size="sm">
-        Start Call
-      </Button>
-      <Button onClick={() => endCall()} size="sm" variant="destructive">
-        End Call
-      </Button>
-      <p>isConnected {`${isConnected}`}</p>
-      <p>isConnecting {`${isConnecting}`}</p>
-      <p>isSpeaking {`${isSpeaking}`}</p>
-      <p>{JSON.stringify(transcript, null, 2)}</p>
-    </div>
-  );
+interface Props {
+  searchParams: Promise<{ organizationId: string }>;
 }
+
+const Page = ({ searchParams }: Props) => {
+  const { organizationId } = use(searchParams);
+  return <WidgetView organizationId={organizationId} />;
+};
+
+export default Page;
