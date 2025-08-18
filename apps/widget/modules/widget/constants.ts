8c487ac9792cd22752ee142f3a1f0fb926a84b10
arka90 <skarka90@gmail.com>
2025-08-18 22:46:56 +0530

    feat: added jotai

---
+++ apps/widget/modules/widget/constants.ts
@@ -0,0 +1,12 @@
+export const WIDGET_SCREENS = [
+  "loading",
+  "auth",
+  "chat",
+  "error",
+  "voice",
+  "contact",
+  "inbox",
+  "selection",
+] as const;
+
+export const CONTACT_SESSION_KEY = "echo_contact_session";
--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,16 +1,29 @@
 "use client";
-
+import { useAtomValue } from "jotai";
 import { WidgetAuthScreen } from "../screens/widget-auth-screen";
+import { screenAtom } from "../../atoms/widget-atoms";
 
 interface Props {
   organizationId: string;
 }
 
 export const WidgetView = ({ organizationId }: Props) => {
+  const screen = useAtomValue(screenAtom);
+
+  const screenComponents = {
+    error: () => <div>Error Screen</div>,
+    loading: () => <div>Loading Screen</div>,
+    auth: () => <WidgetAuthScreen />,
+    chat: () => <div>Chat Screen</div>,
+    voice: () => <div>Voice Screen</div>,
+    contact: () => <div>Contact Screen</div>,
+    inbox: () => <div>Inbox Screen</div>,
+    selection: () => <div>Selection Screen</div>,
+  };
+
   return (
     <main className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-      <WidgetAuthScreen />
-      {/* <WidgetFooter /> */}
+      {screenComponents[screen]()}
     </main>
   );
 };
